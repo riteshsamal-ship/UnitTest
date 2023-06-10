a00cc834dae7f02985dddc78afd12f7737471f75
Ritesh Samal <riteshsamal@outlook.com>
2023-06-09 21:23:30 -0700

    test cases

--- eclipse-workspace/mavenproject/src/main/java/UnitTest/mavenproject/CILab.java
+++ eclipse-workspace/mavenproject/src/main/java/UnitTest/mavenproject/CILab.java
@@ -1,5 +1,35 @@
 package UnitTest.mavenproject;
 
-public class CILab {
+public class CILab implements CILabInterface {
+    private String myString="";
 
+    @Override
+    public String getString() {
+        return myString;
+    }
+
+    @Override
+    public void setString(String string) {
+        myString = string;
+    }
+
+    @Override
+    public boolean detectCapitalUse() {
+        // Check if the usage of capitals in the word is correct
+        if (myString == null) {
+            return false;
+        }
+
+        int capitalCount = 0;
+        int length = myString.length();
+
+        for (int i = 0; i < length; i++) {
+            char c = myString.charAt(i);
+            if (Character.isUpperCase(c)) {
+                capitalCount++;
+            }
+        }
+
+        return capitalCount == 0 || capitalCount == length || (capitalCount == 1 && Character.isUpperCase(myString.charAt(0)));
+    }
 }
--- eclipse-workspace/mavenproject/src/test/java/UnitTest/mavenproject/CILabTest.java
+++ eclipse-workspace/mavenproject/src/test/java/UnitTest/mavenproject/CILabTest.java
@@ -1,5 +1,73 @@
 package UnitTest.mavenproject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class CILabTest {
 
+    private CILab tester;
+    
+    public void setUp() {
+        tester = new CILab();
+    }
+
+    
+    public void tearDown() {
+        tester = null;
+    }
+
+    @Test
+    public void detectCapitalUseTest1() {
+    	setUp();
+        tester.setString("USA");
+        Assertions.assertTrue(tester.detectCapitalUse());
+        tearDown();
+    }
+
+    @Test
+    public void detectCapitalUseTest2() {
+    	setUp();
+        tester.setString("leetcode");
+        Assertions.assertTrue(tester.detectCapitalUse());
+        tearDown();
+    }
+    
+    @Test
+    public void detectCapitalUseTest3() {
+    	setUp();
+        tester.setString("Google");
+        Assertions.assertTrue(tester.detectCapitalUse());
+        tearDown();
+    }
+
+    @Test
+    public void detectCapitalUseTest4() {
+    	setUp();
+        tester.setString("RiteshSamal");
+        Assertions.assertTrue(tester.detectCapitalUse());
+        tearDown();
+    }
+
+    @Test
+    public void detectCapitalUseTest5() {
+    	setUp();
+        tester.setString("hello");
+        Assertions.assertTrue(tester.detectCapitalUse());
+        tearDown();
+    }
+
+    @Test
+    public void detectCapitalUseTest6() {
+    	setUp();
+        tester.setString("WORLD");
+        Assertions.assertFalse(tester.detectCapitalUse());
+        tearDown();
+    }
+
+    @Test
+    public void detectCapitalUseTest7() {
+    	setUp();
+        tester.setString("AbcDEfg");
+        Assertions.assertFalse(tester.detectCapitalUse());
+        tearDown();
+    }
 }
